b642de2aab100986fea68b4779d16c888de9768b
Kyle Coburn <kylecoburn@gmail.com>
2018-08-14 09:31:43 -0700

    ✨ Convert package.json after processing

--- generator.js
+++ generator.js
@@ -1,27 +1,48 @@
 module.exports = (api) => {
+	const REGEX_SPACE_INDENTS = /^(  )+/gm
+	const REGEX_TWO_SPACES = /  /g
+	const PACKAGE_JSON = 'package.json'
+
+	const replaceAllTwoSpacesWithTab = (spaces) => {
+		return spaces.replace(REGEX_TWO_SPACES, '\t')
+	}
+
+	const convertFileIndentation = (fileString) => {
+		const resultString = fileString.replace(REGEX_SPACE_INDENTS, replaceAllTwoSpacesWithTab)
+		return resultString !== fileString ? resultString : null
+	}
+
+	let convertedFiles = []
+
 	api.postProcessFiles(files => {
-		const REGEX_SPACE_INDENT = /^(  )+/gm
-		const REGEX_TWO_SPACES = /  /g
-
-		const tabPerTwoSpaces = (spaces) => {
-			return spaces.replace(REGEX_TWO_SPACES, '	')
-		}
-
-		let madeChanges = false
 		for (const fileName in files) {
+			if (fileName === PACKAGE_JSON) {
+				continue
+			}
 			const fileString = files[fileName]
 			if (typeof fileString !== 'string') {
 				continue
 			}
-			const resultString = fileString.replace(REGEX_SPACE_INDENT, tabPerTwoSpaces)
-			if (resultString !== fileString) {
-				console.log(`Converted ${fileName} to tab indentation.`)
-				madeChanges = true
+			const resultString = convertFileIndentation(fileString)
+			if (resultString) {
 				files[fileName] = resultString
+				convertedFiles.push(fileName)
 			}
 		}
-		if (!madeChanges) {
-			console.log('No files were found with 2-space indentation!')
+	})
+
+	api.onCreateComplete(() => {
+		const fs = require('fs')
+		const pathToPackageJson = api.resolve(PACKAGE_JSON)
+		const fileString = fs.readFileSync(PACKAGE_JSON, 'utf-8')
+		const resultString = convertFileIndentation(fileString)
+		if (resultString) {
+			fs.writeFileSync(pathToPackageJson, resultString, 'utf-8')
+			convertedFiles.push(PACKAGE_JSON)
 		}
+		const confirmationMessage = convertedFiles.length
+			? `Converted indentation: ${convertedFiles.join(', ')}`
+			: 'No files were found with 2-space indentation!'
+		api.exitLog(confirmationMessage, 'done')
 	})
 }
